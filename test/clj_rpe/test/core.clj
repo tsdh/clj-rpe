1e12db25c60e3e3200ff8c1cd0621ec59722f856
Tassilo Horn <tsdh80@googlemail.com>
2012-01-11 16:21:31 +0100

    more tests

--- test/clj_rpe/test/core.clj
+++ test/clj_rpe/test/core.clj
@@ -6,9 +6,18 @@
   [c]
   (rpe-reachables c [rpe-+ 'getSuperclass]))
 
+(defn all-superclasses-reflexive
+  [c]
+  (rpe-reachables c [rpe-* 'getSuperclass]))
+
 (deftest reflection-all-superclasses
   (is (= #{Number Object}
          (all-superclasses Long)))
+  (is (= #{Long Number Object}
+         (all-superclasses-reflexive Long)))
   (is (= #{java.awt.Frame java.awt.Window java.awt.Container
            java.awt.Component java.lang.Object}
-         (all-superclasses javax.swing.JFrame))))
+         (all-superclasses javax.swing.JFrame)))
+  (is (= #{javax.swing.JFrame java.awt.Frame java.awt.Window
+           java.awt.Container java.awt.Component java.lang.Object}
+         (all-superclasses-reflexive javax.swing.JFrame))))
