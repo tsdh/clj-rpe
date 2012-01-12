a573ea7f14a939f4490854bdd65798b7cbf83e60
Tassilo Horn <tsdh80@googlemail.com>
2012-01-12 16:15:42 +0100

    docstrings for core

--- src/clj_rpe/core.clj
+++ src/clj_rpe/core.clj
@@ -21,8 +21,14 @@
 
 (defn rpe
   "Returns the set of objects reachable from `objs' (an object or seq of
-  objects) by via the path description `rpd'."
-  [v rpd]
+  objects) via the path description `rpd'.
+
+  Example: Get all direct and indirect supertypes (superclasses, interfaces)
+  of Long.
+
+    (rpe Long [rpe-+ [rpe-alt 'getSuperclass
+                              'getInterfaces]])"
+  [objs rpd]
   (cond
    (fn? rpd)   (into-oset (mapcat (fn [x]
                                     (into-oset
@@ -31,44 +37,56 @@
                                        (catch clojure.lang.ArityException e
                                          (throw e))
                                        (catch Exception _ nil))))
-                                  (into-oset v)))
-   (coll? rpd) (apply (first rpd) v (rest rpd))
-   :else       (into-oset (mapcat #(--> % rpd) (into-oset v)))))
+                                  (into-oset objs)))
+   (coll? rpd) (apply (first rpd) objs (rest rpd))
+   :else       (into-oset (mapcat #(--> % rpd) (into-oset objs)))))
 
 (defn rpe-seq
-  [start & specs]
-  (let [ss (into-oset start)]
-    (if (seq specs)
-      (recur (rpe ss (first specs))
-             (rest specs))
+  "Regular path sequence starting at `objs' and traversing a path defined by
+  `rpds'."
+  [objs & rpds]
+  (let [ss (into-oset objs)]
+    (if (seq rpds)
+      (recur (rpe ss (first rpds))
+             (rest rpds))
       ss)))
 
 (defn rpe-alt
-  [start & alts]
-  (into-oset (mapcat #(rpe start %) alts)))
+  "Regular path alternative starting at `objs' and traversing all `alts' from
+  there."
+  [objs & alts]
+  (into-oset (mapcat #(rpe objs %) alts)))
 
 (defn rpe-opt
-  [start rpd]
-  (let [ss (into-oset start)]
+  "Regular path option returning the union of `objs' and the objects reachable
+  via `rpd'."
+  [objs rpd]
+  (let [ss (into-oset objs)]
     (into-oset ss (rpe ss rpd))))
 
 (defn rpe-+
-  ([v p]
-     (rpe-+ v p false true))
-  ([v p d skip-v]
-     (let [v  (into-oset v)
-	   n  (rpe (if (false? d) v d) p)
-	   df (clojure.set/difference n v)
-	   sv (if skip-v n (into-oset v n))]
+  "Regular path iteration starting at `objs' iterating `rpd' one or many
+  times."
+  ([objs rpd]
+     (rpe-+ objs rpd false true))
+  ([objs rpd d skip-objs]
+     (let [objs (into-oset objs)
+	   n    (rpe (if (false? d) objs d) rpd)
+	   df   (clojure.set/difference n objs)
+	   sv   (if skip-objs n (into-oset objs n))]
        (if (seq df)
-	 (recur sv p df false)
+	 (recur sv rpd df false)
          sv))))
 
 (defn rpe-*
+  "Regular path iteration starting at `objs' iterating `rpd' zero or many
+  times."
   [objs rpd]
   (rpe-+ objs rpd false false))
 
 (defn rpe-exp
+  "Regular path iteration starting at `objs' iterating `rpd' either exactly `n'
+  times, or at minimum `l' times and at most `u' times."
   ([objs l u rpd]
      {:pre [(<= l u) (>= l 0) (>= u 0)]}
      (loop [i (- u l), s (rpe-exp objs l rpd)]
@@ -85,6 +103,7 @@
        (recur (rpe objs rpd) (dec n) rpd))))
 
 (defn rpe-restr
+  "Regular path restriction filtering `objs' by `pred'."
   [objs pred]
   (into-oset (filter pred objs)))
 
